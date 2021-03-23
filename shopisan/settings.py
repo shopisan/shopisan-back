f0744a23e70286a0d4113f111d74120760a59013
alyson <alyson@jh8.dev>
2021-03-23 17:11:13 +0100

    Changed fields exposed to api for categories

--- shopisan/settings.py
+++ shopisan/settings.py
@@ -117,8 +117,8 @@ REST_FRAMEWORK = {
     'UNICODE_JSON': False,
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication'
     ],
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
