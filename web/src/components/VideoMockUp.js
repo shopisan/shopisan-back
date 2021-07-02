7813ca71de4c12cb4f57197d50eb48b567c0d2e7
Martaly <aly.martin.97@gmail.com>
2021-07-02 17:58:48 +0200

    add double screens

---
+++ web/src/components/VideoMockUp.js
@@ -0,0 +1,18 @@
+import React from "react";
+import useStyles from '../theme';
+
+export default function VideoMockUp(props){
+    const {url} = props;
+    const classes = useStyles();
+
+    return (
+        <>
+            <img className={classes.img} src="static/images/mockup_iphone_vide.png"></img>
+            <div className={classes.contentVideo}>
+                <video className={classes.video1} autoPlay loop>
+                    <source src={url} type="video/mp4"/>
+                </video>
+            </div>
+        </>
+    );
+}
