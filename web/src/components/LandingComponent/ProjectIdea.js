9fedfbbd286fcf93774d3de55921ac399b400e9d
Martaly <aly.martin.97@gmail.com>
2021-07-02 08:53:58 +0200

    add double screens

--- web/src/components/LandingComponent/ProjectIdea.js
+++ web/src/components/LandingComponent/ProjectIdea.js
@@ -8,11 +8,10 @@ export default function ProjectIdea(){
     const classes = useStyles();
     const locale = i18n.language;
 
-
     return(
         <>
             <div className={classes.projectIdea} id="idea">
-                <div>
+                <div className="ml-md-5">
                     <Typography variant="h1" className={classes.h1}>{t('idea.title')}</Typography>
     
                     <Typography variant="body1" className={classes.body1}>{t('idea.text')}</Typography>
@@ -20,9 +19,12 @@ export default function ProjectIdea(){
                     <Typography variant="body1" className={classes.body1}>{t('idea.text2')}</Typography>
                 </div>
        
-                <div className= "d-flex justify-content-center" >
-            
-                    <img className={classes.img} src= "static/images/Iphone_Mockup_2.png"></img>
+                <div className= "d-flex justify-content-center mt-5 mr-lg-5" >
+                    {locale === "fr" ?
+                        <img className={classes.img} src="static/images/double_iphone_mockup.png"></img>
+                            :
+                        <img className={classes.img} src="static/images/mockup_double_iphone_EN.png"></img>
+                    }
                 </div>
             </div> 
         </>
